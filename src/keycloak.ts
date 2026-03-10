8c63cffa41de3d182e25f056559d7957c41a9214
Jean-Baptiste Zeller <esurnir@gmail.com>
2026-03-10 11:38:45 +0100

    feat: initialize Keycloak integration and setup project structure for reproducible case.
    - Added package.json with scripts and dependencies for Vite and TypeScript.
    - Included silent-check-sso.html for silent SSO checks.
    - Created main application structure with TypeScript files for keycloak integration and counter functionality.
    - Added styles for the application in style.css.
    - Included SVG logos for Vite and TypeScript.
    - Configured TypeScript with tsconfig.json for ES2022 and bundler mode.

---
+++ src/keycloak.ts
@@ -0,0 +1,83 @@
+import Keycloak from 'keycloak-js';
+
+const keycloak = new Keycloak({
+    url: "http://localhost:7081",
+    realm: "realm-with-orgs",
+    clientId: "public-client"
+});
+
+async function initializeKeycloak() {
+    try {
+        let organizationScopeParam = 'organization';
+        let organizationName = localStorage.getItem('organization_name');
+        if (organizationName) {
+           organizationScopeParam = `organization:${organizationName}`;
+        }
+
+        const authenticated = await keycloak.init({
+                scope: organizationScopeParam,
+                onLoad: 'check-sso',
+                silentCheckSsoRedirectUri: `${location.origin}/silent-check-sso.html`
+            });
+
+        if (authenticated) {
+            const organizationScope = keycloak.idTokenParsed?.organization;
+            if (organizationScope) {
+                organizationName = Object.keys(organizationScope)[0];
+                localStorage.setItem('organization_name', organizationName);
+            }
+            else {
+                console.warn('Organization scope not found in token. Clearing stored organization name and clear the login state');
+                localStorage.removeItem('organization_name');
+                keycloak.clearToken();
+            }
+        }
+        displayUserStatus();
+    } catch (error) {
+        console.error('Failed to initialize adapter:', error);
+    }
+}
+
+function displayUserStatus() {
+        const readTheDoc = document.querySelector<HTMLParagraphElement>('.read-the-docs');
+        if (keycloak.authenticated) {
+            const name = keycloak.tokenParsed?.name || keycloak.tokenParsed?.preferred_username || 'Unknown User';
+            const organizationScope = keycloak.idTokenParsed?.organization;
+            const organizationName = organizationScope ? Object.keys(organizationScope)[0] : 'Unknown Organization';
+            const message = `Hello, ${name}! You are authenticated. Your organization is ${organizationName}.`;
+            if (readTheDoc) {
+                readTheDoc.textContent = message;
+            }
+            console.log('User is authenticated');
+            console.log(message);
+        } else {
+            if (readTheDoc) {
+                readTheDoc.textContent = 'You are not authenticated. Please log in.';
+            }
+            console.log('User is not authenticated');
+        }
+}
+function getOrganizationScopeName() {
+    let organizationScopeParam = 'organization';
+        let organizationName = localStorage.getItem('organization_name');
+        if (organizationName) {
+            organizationScopeParam = `organization:${organizationName}`;
+        }
+        return organizationScopeParam;
+}
+export function loginWithSavedOrg() {
+    return keycloak.login(
+    {
+      scope: getOrganizationScopeName()
+    }
+  )
+}
+export function loginWithoutSavedOrg() {
+    return keycloak.login(
+        {
+            scope: 'organization'
+        }
+    )
+}
+initializeKeycloak();
+export default keycloak;
